675b0d0aa396f222650920adf0b56c27e1249f6a
Gilberto625 <86282769+Gilberto625@users.noreply.github.com>
2025-10-31 14:05:36 -0600

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://backendbina-1.onrender.com',
+
+  firebase: {
+    apiKey: "AIzaSyAJ0Om_GyOwpAgJoaQc7g1oplyGx7g70LQ",           // ← PEGA TU apiKey
+    authDomain: "auth-backend-tu-nombre.firebaseapp.com",        // ← PEGA TU authDomain
+    projectId: "auth-backend-tu-nombre",                         // ← PEGA TU projectId
+    storageBucket: "auth-backend-tu-nombre.firebasestorage.app", // ← PEGA TU storageBucket
+    messagingSenderId: "370925550099",                           // ← PEGA TU messagingSenderId
+    appId: "1:370925550099:web:ebfdea93f12c7b01435de6"          // ← PEGA TU appId
+  }
+};
