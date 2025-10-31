f7c6982ade5df09d70f9b0a192b1d7e3cc184d38
Gilberto625 <86282769+Gilberto625@users.noreply.github.com>
2025-10-31 14:45:55 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://backendbina-1.onrender.com',
+  apiUrl: 'https://backendbina-1.onrender.com/api/usuarios',
 
   firebase: {
     apiKey: "AIzaSyAJ0Om_GyOwpAgJoaQc7g1oplyGx7g70LQ",           // ← PEGA TU apiKey
@@ -10,4 +10,5 @@ export const environment = {
     messagingSenderId: "370925550099",                           // ← PEGA TU messagingSenderId
     appId: "1:370925550099:web:ebfdea93f12c7b01435de6"          // ← PEGA TU appId
   }
+
 };
